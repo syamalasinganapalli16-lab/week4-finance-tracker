89cd91b8004ffd2b854c3fd6ca93c2aed086e1eb
syamalasinganapalli16-lab <syamalasinganapalli16@gmail.com>
2026-02-05 11:18:49 +0530

    Create run.py

---
+++ run.py
@@ -0,0 +1,8 @@
+finance_tracker/__init__.py
+finance_tracker/main.py
+finance_tracker/expense.py
+finance_tracker/expense_manager.py
+finance_tracker/file_handler.py
+finance_tracker/reports.py
+data/expenses.json
+README.md
